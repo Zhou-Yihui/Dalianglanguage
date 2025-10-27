a09fe5caee5b983056130c299d6d74dac5721ef4
Zhou-Yihui <3193203752@qq.com>
2025-10-27 13:44:01 +0800

    Create KeyboardRenderer.kt

---
+++ app/src/main/java/com/lyurchxieria/stargateime/KeyboardRenderer.kt
@@ -0,0 +1,9 @@
+package com.lyurchxieria.stargateime
+
+import android.content.Context
+import android.inputmethodservice.KeyboardView
+import android.util.AttributeSet
+
+class KeyboardRenderer(context: Context, attrs: AttributeSet? = null) : KeyboardView(context, attrs) {
+    // 这里渲染星门字符按键，可根据 stargate_chars.json 加载
+}
